756f0d58529f019f4126e30f05e0e8b988a75909
dajeong11 <dajeongkim.mail@gmail.com>
2024-04-25 01:02:51 +0900

    alter quiz.py

--- quiz.py
+++ quiz.py
@@ -12,13 +12,15 @@ class Quiz:
         while self.quiz_dict:
             len_dict = len(self.quiz_dict)
             r_num = random.randrange(len_dict)
+            problem_list = list(self.quiz_dict.items())
             print(f"#{count}")
-            question, answer = self.quiz_dict.popitem()
+            question, answer = problem_list[r_num][0], problem_list[r_num][1]
             print(question)
             user_answer = input("정답입력: ")
             print(answer)
             if user_answer.lower() == answer.lower():
                 print("정답!")
+                problem_list.pop(r_num)
             else:
                 print("오답!")
             print("------------------------------------")
